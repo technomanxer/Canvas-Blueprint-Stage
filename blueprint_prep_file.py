85fac04d99ce1ccbe4414e6e2fda982fffacb5e1
Melody Lam <101196974+technomanxer@users.noreply.github.com>
2023-10-02 18:02:34 -0400

    Update blueprint_prep_file.py with notes

--- blueprint_prep_file.py
+++ blueprint_prep_file.py
@@ -18,43 +18,49 @@ headers={
     'Authorization': "Bearer " + keys['prod']
 }
 
-req = requests.post('https://cms.instructure.com/api/v1/accounts/1446/reports/sis_export_csv', params={'parameters[courses]': True}, headers=headers)
+#Edit the request so that your URL is used 
+
+req = requests.post('https:/<insert URL here>/api/v1/accounts/1446/reports/sis_export_csv', params={'parameters[courses]': True}, headers=headers)
 json_re = req.json()
 time.sleep(10)
 
 id = json_re['id']
 print("report " + str(id))
 
-#check url
+#check url for if the report is done.
 done = False
 file_url=""
 while(not done):
-    req = requests.get('https://cms.instructure.com/api/v1/accounts/1446/reports/sis_export_csv/'+str(id), headers=headers)
+    req = requests.get('https://<insert URL here>/api/v1/accounts/1446/reports/sis_export_csv/'+str(id), headers=headers)
     print("not done yet, sleeping for 30 secs")
     if(req.json()['status'] == 'complete'):
         file_url = req.json()['attachment']['url']
         break
     time.sleep(30)
 
-#download url
+#download the report!
 req = requests.get(file_url)
 open("sis_export.csv", 'wb').write(req.content)
 
+
+#Pull courses that have blueprints associated.
+#see course_codes.csv for blueprint file mapping
 need_blueprint=[]
 with open('course_codes.csv') as csvfile:
     read = csv.reader(csvfile)
     need_blueprint=[x for x in read if x[-1]]
 
 
-#dictionary formation 
+#dictionary formation {course_code: blueprint}
 associate = dict()
 for rows in need_blueprint:
     associate[rows[0]] = rows[-1]
 
-associate['99329X0900'] = 'titleixhs' #homeroom HS
-associate['99329Y0900'] = 'titleixms' #homeroom MS
+#Use this area to put in custom course codes and blueprints. Or add to the file course_codes.CSV
+#associate['99329X0900'] = 'titleixhs' #homeroom HS
+#associate['99329Y0900'] = 'titleixms' #homeroom MS
 
-#feed bp file
+#This now reads the export file and determines who needs a blueprint association
 no_assoc = []
 with open('sis_export.csv') as sis_file:
     read = csv.DictReader(sis_file)
@@ -63,20 +69,21 @@ with open('sis_export.csv') as sis_file:
 #course code: no_assoc[0]['short_name'][-11:-1]
 sis_import = []
 
+# now we write the association into the end of the line in the CSV and output a new file called "sis_import"
 for c in no_assoc:
     course_code = c['short_name'][-11:-1]
     if course_code in associate.keys():
         c['blueprint_course_id'] = "bp_" + associate[course_code]
         sis_import.append(c)
 
+#write to import file with today's date
 filename='sis_import_'+str(datetime.date.today())+'.csv'
 with open(filename, 'w',  newline="") as import_file:
     write_f = csv.DictWriter(import_file, fieldnames=sis_import[0].keys())
     write_f.writeheader()
     write_f.writerows(sis_import)
 
-# to do write code to upload
-
+# Uploads file to Canvas instance
 file_send = {"attachment": open(filename, 'rb')}
-req = requests.post("https://cms.instructure.com/api/v1/accounts/1/sis_imports", headers=headers, files=file_send)
+req = requests.post("https://<insert instance here>/api/v1/accounts/1/sis_imports", headers=headers, files=file_send)
 print(req.json())
